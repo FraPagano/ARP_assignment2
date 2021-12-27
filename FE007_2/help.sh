1f6f91ab08dd4797462fce27d8efc6ce8822f8e8
Your Name <you@example.com>
2021-12-27 13:07:50 +0100

    readme1

--- FE007_2/help.sh
+++ FE007_2/help.sh
@@ -2,9 +2,6 @@ more -d ./general_instructions.txt;
 
 more -d ./src/master/master.txt;
 
-more -d ./src/parameters/parameters.txt;
-
-more -d ./src/timing/timing.txt;
 
 more -d ./src/producer_named/producer_named.txt;
 more -d ./src/consumer_named/consumer_named.txt;
@@ -18,4 +15,7 @@ more -d ./src/consumer_socket/consumer_socket.txt;
 more -d ./src/producer_shm/producer_shm.txt;
 more -d ./src/consumer_shm/consumer_shm.txt;
 
+more -d ./src/parameters/parameters.txt;
+
+more -d ./src/timing/timing.txt;
 
